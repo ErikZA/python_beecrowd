e8315eca805ac5a69a2913a0a189ff226787d0ca
Erik hz <erikzambeli@gmail.com>
2023-04-03 22:24:49 -0300

    fix: friends bug

--- friends.py
+++ friends.py
@@ -1,24 +1,23 @@
-inscricao = input()
-nome, escolha = inscricao.split()
-
-vencedor = {"nome": nome, "nome_tam": len(nome)}
-inscritos_yes = set()
-inscritos_no = set()
-
-while inscricao != 'FIM':
-    nome, escolha = inscricao.split()
-    if escolha == 'YES':
-        inscritos_yes.add(nome)
-        if len(nome) > vencedor["nome_tam"]:
-            vencedor["nome"] = nome
-            vencedor["nome_tam"] = len(nome)
+s_y = set()
+s_n = set()
+a = 0
+ans = ''
+while True:
+    s = input()
+    if s == 'FIM':
+        break
+    if s.__contains__('YES'):
+        s_y.add(s.split()[0])
     else:
-        inscritos_no.add(nome)
-    inscricao = input()
-
-inscritos_yes = sorted(inscritos_yes)
-inscritos_no = sorted(inscritos_no)
-
-print("\n".join(inscritos_yes))
-print("\n".join(inscritos_no))
-print(f"\nAmigo do Habay:\n{vencedor['nome']}")
+        s_n.add(s.split()[0])
+    if a == 0:
+        a = len(s.split()[0])
+    elif a < len(s.split()[0]) and s.__contains__('YES'):
+        a = len(s.split()[0])
+        ans = s.split()[0]
+for item in sorted(s_y):
+    print(item)
+for item in sorted(s_n):
+    print(item)
+print("\nAmigo do Habay:")
+print(ans)
